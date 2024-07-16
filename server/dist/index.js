29fa6098d0a8f06c5de310ac171c013e1b6e4f88
Joris Mansion <juhh@juhh-core.lan>
2024-07-16 14:18:47 +0200

    init apollo server

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,18 +2,15 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { getAllProjects } from "./src/services/project.js";
 import typeDefs from "./schema.js";
-
 const resolvers = {
-  Query: {
-    projects: () => getAllProjects(),
-  },
+    Query: {
+        projects: () => getAllProjects(),
+    },
 };
-
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
 });
-
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+    listen: { port: parseInt(process.env.PORT) || 4000 },
 });
---
+++ server/dist/schema.js
@@ -0,0 +1,11 @@
+const typeDefs = `#graphql
+type Project {
+  id: ID!
+  title: String!
+  description: String
+}
+type Query {
+    projects: [Project]
+}
+`;
+export default typeDefs;
